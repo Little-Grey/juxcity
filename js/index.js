5e103e877416c3e7bab73d73921b412af03667d9
grey <15920111346@163.com>
2018-05-09 11:46:31 +0800

    完成角色管理页面

--- js/index.js
+++ js/index.js
@@ -1,36 +1,48 @@
 $(function () {
-    console.log(2222222);
+    // 退出登录
+    $('.pc_quit s').on('click',function(){
+        var r = confirm("确认退出?")
+        if (r == true) {
+            alert('退出成功');
+        };
+    })
+
+    // 导航的索引/
     $('.nav_ul li dl dd').each(function (i) {
         $(this).on('click', function () {
+            sessionStorage.setItem('index', i);
             change(i);
         })
     })
+    // sessionStorage
+    $(document).ready(function () {
 
+        // 调用
+        getHeight('.add_administrator', '.layui-bg-cyan');
+        getHeight('#outer', '.layui-bg-cyan');
+
+        var user = JSON.parse(sessionStorage.getItem('index'))
+        change(user);
+    });
 
     //导航tab切换,show,hied
     function change(temp_i) {
-        $("#outer").children().eq(temp_i).show();
-        $("#outer").children().eq(temp_i).siblings().hide()
+        $("#outer").children().eq(temp_i).show().siblings().hide();
+        // 导航的点击索引
+        $('.nav_ul li dl dd').eq(temp_i).addClass('layui-this').siblings().removeClass('layui-this');
+
     };
 
 
+    // $('.nav_ul li').each(function(i){
+    //     // console.log(i);
+    //     $(this).on('click',function(){
+    //         // console.log(i)
+    //         sessionStorage.setItem('index', i);
+    //         $('.nav_ul li').eq(i).addClass('layui-nav-itemed');
+    //     })
+    // })
 
-
-
-
-
-
-    // 搜索按钮的触发
-    $('.btn_two').on('click', function () {
-        console.log(1);
-    })
-
-
-
-
-    // 调用
-    getHeight('.add_administrator', '.layui-bg-cyan');
-    getHeight('#outer', '.layui-bg-cyan');
 
     /**
      * 封装了一个获取到整个屏幕的高度.100vh单位系获取整个屏幕的宽度,
@@ -40,12 +52,42 @@ $(function () {
      */
     function getHeight(box1, box2) {
         $(box1).css({
-            'height': $(window).height() - $(box2).height()
+            'height': $(window).height() - $(box2).height() - $('.crumbs').height()
         });
     }
 
-    $(window).resize(function () {
-        getHeight('.d2', '.d1');
+
+
+
+    // 点击新增管理员弹出对应页面
+    $('.btn_two').on('click', function () {
+        $('.add_administrator').show();
     });
+
+    // 点击新增管理员页面返回,返回上一层
+    $('.back_admin').on('click', function () {
+        var r = confirm("确认返回?")
+        if (r == true) {
+            $('.add_administrator').hide();
+        };
+    });
+
+    // 点击增加角色弹出对应页面-添加角色页面
+    $('.part').on('click',function(){
+        $('.add_role').show();
+    })
+    // 点击新添加角色页面返回,返回上一层
+    $('.back_part').on('click',function(){
+        var r = confirm("确认返回?")
+        if (r == true) {
+            $('.add_role').hide();
+        };
+    })
+
+
+
+
+
+
 
 });
